e55bba884aa28faf8008ff4d5807821a70c7270e
josuecastro <josue.castro@upr.edu>
2019-05-05 16:50:07 -0400

    main working, all set up

--- main.py
+++ main.py
@@ -1,54 +1,119 @@
 import os, sys, time
 from cfonts import say
-from colorama import Fore, Back, init
+from colorama import Fore, Back, Style
 from board import Board
 from agent import Agent
 
+DEFAULT_LEVEL = 1
+DEFAULT_DEPTH = 2
+
+def exit_app():
+    print("Exit!")
+    time.sleep(1)
+    print(Style.RESET_ALL)
+    os.system('clear')
+    sys.exit(0)
+
+def clean_line(n):
+    time.sleep(1)
+    for x in range(n):
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
+
+def valid_play(user_input):
+    if len(user_input.split()) != 2:
+        return []
+    for x in user_input.split():
+        if not x.isnumeric():
+            return []
+    return [int(x) for x in user_input.split()]
+
+def configure_agent(color):
+    print("DEFAULT=1".rjust(40), flush=True, end='\r')
+    level = input("%s's difficulty (1-3): " % color)
+    while not level.isnumeric():
+        print("Try a Valid Option!")
+        clean_line(2)
+        level = input("%s's difficulty (1-3): " % color)
+    level = int(level) if 0 < int(level) <= 3 else DEFAULT_LEVEL
+    print("DEFAULT=1".rjust(40), flush=True, end='\r')
+    depth = input("%s's depth (1-6): " % color)
+    while not depth.isnumeric():
+        print("Try a Valid Option!")
+        clean_line(2)
+        depth = input("%s's depth (1-6): " % color)
+    depth = int(depth) if 0 < int(depth) <= 6 else DEFAULT_DEPTH
+    os.system('clear')
+    return level, depth
+
 
 if __name__ == '__main__':
+    # Setting up menu and options
     print(Back.CYAN)
     os.system('clear')
     say("Othello", font='block', align='center', colors=['white', 'black'])
     print(Fore.BLACK)
-    print("1. Player vs Player\n2. Player vs AI\n3. AI vs Player\n4. AI vs AI")
-    menu = int(input("Select: "))
-    os.system('clear')
-    level = depth = 3
+    print("1. Player vs Player\n2. Player vs AI\n3. AI vs Player\n4. AI vs AI\n")
+    print("\t\tPress ENTER to Exit", flush=True, end='\r')
+    menu = input("Select: ")
+    while True:
+        if not menu:
+            exit_app()
+        elif menu.isnumeric() and 0 < int(menu) <= 4:
+            menu = int(menu)
+            break
+        print("Enter a Valid Option!")
+        clean_line(2)
+        menu = input("Select: ")
     players = {}
-
+    os.system('clear')
     if menu == 1:
         pass
     elif menu == 2:
-        players['O'] = Agent(Board.WHITE, level, depth)
+        # configure agent
+        level, depth = configure_agent('White')
+        players['O'] = Agent(Board.WHITE, level, depth), []
     elif menu == 3:
-        players['@'] = Agent(Board.WHITE, level, depth)
+        level, depth = configure_agent('Black')
+        players['@'] = Agent(Board.WHITE, level, depth), []
     elif menu == 4:
-        players['@'] = Agent(Board.WHITE, 2, depth)
-        players['O'] = Agent(Board.WHITE, level, depth)
+        level, depth = configure_agent('Black')
+        players['@'] = Agent(Board.WHITE, level, depth), []
+        level, depth = configure_agent('White')
+        players['O'] = Agent(Board.WHITE, level, depth), []
 
     board = Board()
     turn = board.BLACK
     board.draw_board(turn)
+    # Game flow
     while not board.end_of_game():
-        if turn in players:
-            agent = players[turn]
+        if turn in players:  # its an agent's turn
+            agent = players[turn][0]
             print(turn+" agent's turn")
-            time.sleep(1)
-            move = agent.negamax_AB(turn, board, 3, agent.MIN, agent.MAX)[1]
-            board.make_move(move, turn)
-        else:
+            time.sleep(1)  # simulate agent's thinking time
+            start = time.time()  # measure time
+            move = agent.negamax_AB(turn, board, agent.depth, Agent.MIN, Agent.MAX)[1]
+            end = time.time()
+            players[turn][1].append(end-start)
+        else:  # user turn
             while True:
-                move = input(turn+"'s turn (row col) > ").split()
-                move = tuple(map(int, move))
-                if board.is_legal(move, turn):
+                move = input(turn+"'s turn (row col)> ")
+                if not move:
+                    exit_app()
+                move = tuple(valid_play(move))
+                if move and board.is_legal(move, turn):
                     board.make_move(move, turn)
                     break
                 else:
-                    print("Try a valid move:", board.legal_moves(turn))
+                    print("Try a valid move!")
+                    clean_line(2)
+        board.make_move(move, turn)
         os.system('clear')
         prev_turn = turn
         turn = board.next_turn(turn)
         board.draw_board(turn)
         print(prev_turn+"'s move", move)
     print("Game Over!\n")
-    sys.exit(0)
+    for player in players:
+        print("Player %s avg = %dms, max = %dms" % (player, sum(players[player][1]) * 1000 / len(players[player][1]), max(players[player][1]) * 1000))
+    exit_app()
